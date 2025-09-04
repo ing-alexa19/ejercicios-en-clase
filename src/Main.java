359c6608538e398ef59b101edad5092aa39c5e8e
alexa <navarretecangrejocarenalexandr@gmail.com>
2025-09-04 13:27:22 -0500

    cambios

---
+++ src/Main.java
@@ -0,0 +1,79 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        //ejercicio1
+/*
+        int n1, n2, suma;
+        System.out.println("digite el 1 valor");
+        n1= teclado.nextInt();
+        System.out.println("digite el  2 valor ");
+        n2= teclado.nextInt();
+        suma=n1+n2;
+        System.out.println("la suma es:"+suma);
+*/
+
+        // ejercicio2
+        /*
+            Random ale =new Random();
+            int n1,n2,suma;
+            n1 = ale.nextInt(51);
+            System.out.println(n1);
+            n2 = ale.nextInt(51);
+            System.out.println(n2);
+            suma = n1 + n2;
+            System.out.println(suma);
+*/
+
+
+
+
+
+        // ejercicio 3
+/*
+                    int i = 0;
+                    int n1;
+                    int suma=0;
+                    while (i <= 100) {
+                        Random ale = new Random();
+                        n1 = ale.nextInt((200 - 1) + 1) + 1;
+                        System.out.println(n1);
+                        i++;
+                        suma = n1+suma;
+                    }
+                    System.out.println("La suma de los números es: "+suma);
+                }
+            }
+*/
+
+
+        //ejercicio 4
+      /*
+                    int i = 0;
+                    int n1;
+                    int suma = 0;
+                    int suma1 = 0;
+                    while (i <= 100) {
+                        Random ale = new Random();
+                        n1 = ale.nextInt((200 - 1) + 1) + 1;
+                        System.out.println(n1);
+                        i++;
+                        if (n1 % 2 == 0) {
+                            suma1 = n1 + suma1;}
+                        else
+                            suma = n1 +suma;
+                    }
+                    System.out.println("La suma de los números pares es: " + suma1);
+                    System.out.println("La suma de los números impares es: " + suma);
+                    */
+
+                }
+            }
+
+
+
+
+
